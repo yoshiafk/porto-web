b94d5282c7512fb9ad024361afe4ab5aee005ddf
yoshiafk <shayr210@gmail.com>
2020-12-27 18:40:22 +0700

    adding readmore function

--- assets/script.js
+++ assets/script.js
@@ -57,3 +57,20 @@ function sendMail() {
     encodeURIComponent(document.getElementById(myText).value);
   window.location.href = link;
 }
+
+//Read More
+function readMore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("readBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
