c4875325770f9dfd9965167f4de999cb9662ca10
yoshiafk <shayr210@gmail.com>
2020-12-27 17:27:02 +0700

    adding files

---
+++ assets/script.js
@@ -0,0 +1,59 @@
+//Sticky Navbar
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+    if (this.scrollY > 500) {
+      $(".scroll-up-btn").addClass("show");
+    } else {
+      $(".scroll-up-btn").removeClass("show");
+    }
+  });
+
+  //Slide-UP
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
+
+  //Toggle menu/navbar
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+});
+
+//Link in Card
+$(document).ready(function () {
+  $("[data-link]").click(function () {
+    window.open((window.open.href = $(this).attr("data-link")));
+    return false;
+  });
+});
+
+//Typing Animation
+var typed = new Typed(".typing", {
+  strings: ["Front-End Developer", "Graphic Designer", "Photographer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
+var typed = new Typed(".typing2", {
+  strings: ["Front-End Developer", "Graphic Designer", "Photographer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
+
+//Send mail
+function sendMail() {
+  var link =
+    "mailto:yosyfakry@gmail.com" +
+    "&subject=" +
+    encodeURIComponent(document.getElementById(mySubject).value) +
+    "&body=" +
+    encodeURIComponent(document.getElementById(myText).value);
+  window.location.href = link;
+}
